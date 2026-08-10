29f99ad73e3e83a4c2b72e8a15679e1e881da17f
rlgcz <1427449327@qq.com>
2026-08-10 21:42:39 +0800

    Create warps.ts

---
+++ packages/common/i18n/zh-CN/warps.ts
@@ -0,0 +1,11 @@
+export default {
+    WARPED_TO: '你已传送至 {{name}}。',
+
+    CANNOT_WARP_ACHIEVEMENT: '该传送点需要完成对应成就才可使用。',
+    CANNOT_WARP_QUEST: '你必须完成任务 {{questName}}，才能传送到 {{name}}。',
+    CANNOT_WARP_LEVEL: '你的等级至少达到 {{level}} 才可传送到此处。',
+    CANNOT_WARP_JAIL: '你处于监禁状态，还需等待 {{time}}，无法进行传送。',
+    CANNOT_WARP_TUTORIAL: '需要完成新手教程才可以使用传送。',
+    CANNOT_WARP_COMBAT: '战斗结束后需要等待10秒才能传送。',
+    CANNOT_WARP_COOLDOWN: '还需要等待 {{time}} 才能再次传送。'
+} as const;
