9275b6569f0f2a1ba43bea663b33c74dce0b425b
rlgcz <1427449327@qq.com>
2026-08-10 21:24:42 +0800

    Update item.ts

--- packages/common/i18n/zh-CN/item.ts
+++ packages/common/i18n/zh-CN/item.ts
@@ -1,7 +1,7 @@
 export default {
-    CANNOT_ACCESS_LOOTBAG: 'You cannot access this lootbag right now.',
-    REQUIRES_ACHIEVEMENT: 'This item requires a secret achievement to equip.',
-    REQUIRES_QUEST: 'You must complete {{quest}} to equip this item.',
-    SKILL_LEVEL_REQUIREMENT_EQUIP: 'Your {{skill}} must be at least level {{level}} to equip this.',
-    TOTAL_LEVEL_REQUIREMENT: 'Your total level must be at least {{level}} to equip this item.'
+    CANNOT_ACCESS_LOOTBAG: '你当前无法打开这个战利品包裹。',
+    REQUIRES_ACHIEVEMENT: '装备该物品需要解锁一项隐藏成就。',
+    REQUIRES_QUEST: '你必须完成任务 {{quest}} 才能装备此物品。',
+    SKILL_LEVEL_REQUIREMENT_EQUIP: '你的 {{skill}} 技能等级至少达到 {{level}} 才可装备。',
+    TOTAL_LEVEL_REQUIREMENT: '你的总等级至少达到 {{level}} 才可装备该物品。'
 } as const;
