bfc05bf1f5b92f97c69dafd4ec8c400b083b1b13
rlgcz <1427449327@qq.com>
2026-08-10 21:34:24 +0800

    Update crafting.ts

--- packages/common/i18n/zh-CN/crafting.ts
+++ packages/common/i18n/zh-CN/crafting.ts
@@ -1,8 +1,8 @@
 export default {
-    INVALID_DATA: 'Invalid crafting data, please submit a bug report.',
-    INVALID_ITEM: 'Invalid item selected.',
-    INVALID_ITEMS: 'You do not have the required items to craft that.',
-    SELECT_ITEM: 'Please select an item to craft.',
-    FAILED_CRAFT: 'You have failed to craft {{failedText}}.',
-    FAILED_CRAFT_ONE: ' You have failed to craft the item.'
+    INVALID_DATA: '合成数据异常，请提交漏洞反馈。',
+    INVALID_ITEM: '选中的物品无效。',
+    INVALID_ITEMS: '你没有该合成所需的材料。',
+    SELECT_ITEM: '请选择要合成的物品。',
+    FAILED_CRAFT: '{{failedText}} 合成失败。',
+    FAILED_CRAFT_ONE: ' 物品合成失败。'
 } as const;
