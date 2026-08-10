906e2ba1863bdf4c7d8895a3bffc9e3027557cc9
rlgcz <1427449327@qq.com>
2026-08-10 21:37:05 +0800

    Update enchant.ts

--- packages/common/i18n/zh-CN/enchant.ts
+++ packages/common/i18n/zh-CN/enchant.ts
@@ -1,7 +1,7 @@
 export default {
-    NO_ITEM_SELECTED: 'You have not selected an item to enchant.',
-    CANNOT_ENCHANT: 'The item you have selected cannot be enchanted.',
-    NO_SHARD: 'You must provide a shard to enchant an item.',
-    FAILED_ENCHANT: 'You tried enchanting but nothing happened.',
-    SUCCESSFUL_ENCHANT: 'You successfully enchanted your item.'
+    NO_ITEM_SELECTED: '你尚未选择要附魔的物品。',
+    CANNOT_ENCHANT: '选中的物品无法进行附魔。',
+    NO_SHARD: '进行附魔需要提供附魔碎片。',
+    FAILED_ENCHANT: '附魔尝试完成，但没有产生任何效果。',
+    SUCCESSFUL_ENCHANT: '物品附魔成功。'
 } as const;
