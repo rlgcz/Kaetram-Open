91d5d95fd7692c4cb88d2d678c695b7edd6b7f5e
rlgcz <1427449327@qq.com>
2026-08-10 21:21:02 +0800

    Create resource.ts

---
+++ packages/common/i18n/zh-CN/resource.ts
@@ -0,0 +1,19 @@
+export default {
+    MUST_EQUIP_AXE: '你必须装备斧头才可以砍伐树木。',
+    MUST_EQUIP_PICKAXE: '你需要手持镐子才能开采矿石。',
+    MUST_EQUIP_FISHING: '你需要手持渔具才可以进行垂钓。',
+    MUST_EQUIP_FORAGING: '你需要专用工具才能采集该资源。',
+
+    INVALID_LEVEL_LUMBERJACKING: '伐木技能等级需要达到 {{level}}，才能砍伐这棵树。',
+    INVALID_LEVEL_MINING: '采矿技能等级需要达到 {{level}}，才能开采这块矿石。',
+    INVALID_LEVEL_FISHING: '钓鱼技能等级需要达到 {{level}}，才可以在此处垂钓。',
+    INVALID_LEVEL_FORAGING: '采集技能等级需要达到 {{level}}，才能收获该资源。',
+
+    UNABLE_TO_INTERACT_LUMBERJACKING: '目前你无法砍伐这棵树木。',
+    UNABLE_TO_INTERACT_MINING: '你尚不掌握开采该矿石所需的相关技艺。',
+    UNABLE_TO_INTERACT_FISHING: '目前你无法在此处垂钓。',
+    UNABLE_TO_INTERACT_FORAGING: '目前你无法采集该资源。',
+
+    NO_REASON_TREE: '你没有理由砍伐这棵树。',
+    NO_REASON_FISHING: '你没有理由在此处钓鱼。'
+} as const;
